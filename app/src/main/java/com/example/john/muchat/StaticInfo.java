b0d3deb81b6ce127d21ba247e1ef0fe47ef57e4a
Jdroida <z867793137@icloud.com>
2017-05-17 20:03:17 +0800

    start

---
+++ app/src/main/java/com/example/john/muchat/StaticInfo.java
@@ -0,0 +1,12 @@
+package com.example.john.muchat;
+
+/**
+ * Created by John on 2017/5/17.
+ */
+
+public class StaticInfo {
+    public static final String getuiAppID="KkQPxYEsw99WbeiHiOcoN7";
+    public static final String getuiAppSecret="C0mQjT6g8772f4rNG3fIk9";
+    public static final String getuiAppKey="bDBs2GWdkl9B56YL5GwCk1";
+    public static final String getuiMasterSecret="06o4u7OdCR6W0umZaBKVr4";
+}

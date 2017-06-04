bce633bb6567f52d26a1a4980e034bd75eaadae1
Jdroida <z867793137@icloud.com>
2017-06-04 17:57:09 +0800

    第四章完结撒花！

---
+++ app/src/main/java/com/example/john/muchat/activities/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.john.muchat;
+
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+import com.example.common.app.Activity;
+import com.example.john.muchat.activities.AccountActivity;
+import com.example.john.muchat.assist.PermissionsFragment;
+import com.example.john.muchat.fragments.main.ActiveFragment;
+import com.example.john.muchat.fragments.main.ContactFragment;
+import com.example.john.muchat.fragments.main.GroupFragment;
+import com.example.john.muchat.helper.Navhelper;
+
+import net.qiujuer.genius.ui.Ui;
+import net.qiujuer.genius.ui.widget.FloatActionButton;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener
+        , Navhelper.OnTabChangeListener<Integer> {
+    @BindView(R.id.action)
+    FloatActionButton action;
+    @BindView(R.id.appbar)
+    View appbar;
+    @BindView(R.id.portrait)
+    View portrait;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.layout_container)
+    FrameLayout container;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+
+    @Override
+    protected int getContentLayoutID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        navhelper = new Navhelper<>(getSupportFragmentManager(), R.id.layout_container,
+                this,this);//初始化工具类
+        navhelper.add(R.id.action_home, new Navhelper.Tab<>(ActiveFragment.class, R.string.title_home))
+                .add(R.id.action_group, new Navhelper.Tab<>(GroupFragment.class, R.string.title_group))
+                .add(R.id.action_contact, new Navhelper.Tab<>(ContactFragment.class, R.string.title_contact));
+        navigation.setOnNavigationItemSelectedListener(this);
+        Glide.with(this).load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(appbar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                        //this指的是view Target
+                    }
+                });
+        PermissionsFragment.haveAllPermissions(this,getSupportFragmentManager());
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        Menu menu=navigation.getMenu();
+        menu.performIdentifierAction(R.id.action_home,0);//触发首次选中home
+    }
+
+    @OnClick(R.id.search)
+    void onSearchMenuClick() {
+
+    }
+
+    @OnClick(R.id.action)
+    void onActionClick() {
+        AccountActivity.show(this);
+    }
+
+    private Navhelper<Integer> navhelper;
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return navhelper.performClickMenu(item.getItemId());
+    }
+
+    @Override
+    public void onTabChanged(Navhelper.Tab<Integer> newTab, Navhelper.Tab<Integer> oldTab) {
+        title.setText(newTab.extra);//从额外字段中取出title资源ID
+        //设置浮动按钮动画
+        float transY=0;
+        float rotation=0;
+        if(Objects.equals(newTab.extra,R.string.title_home)){
+            transY= Ui.dipToPx(getResources(),76);//主界面时隐藏
+        }else{
+            if(Objects.equals(newTab.extra,R.string.title_group)){
+                action.setImageResource(R.drawable.ic_group_add);
+                rotation=-360;
+            }else{
+                action.setImageResource(R.drawable.ic_contact_add);
+                rotation=360;
+            }
+        }
+        //开始动画，先旋转再竖直平移，附带弹性效果（插值器）
+        action.animate().rotation(rotation)
+                .translationY(transY)
+                .setDuration(480)
+                .setInterpolator(new AnticipateOvershootInterpolator(1))
+                .start();
+    }
+}
